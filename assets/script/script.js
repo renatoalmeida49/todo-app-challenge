a9bb9dec167bf8a3d9ca8c15b0275b3701c17a0e
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-03 20:13:28 -0300

    Changing the style of the activities

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,15 +6,20 @@ function fetchData() {
         .then(response => response.json())
         .then(response => {
             activities = response.results
+            mountPage()
         })
 }
 
 function mountPage() {
     activities.forEach(activity => {
         let div = document.createElement('div')
-        div.classList.add('activity')
+        let paragraph = document.createElement('p')
+        let circle = document.createElement('div')
 
-        div.innerText = activity.title
+        div.classList.add('activity')
+        circle.classList.add('circle')
+
+        paragraph.innerText = activity.title
 
         switch(activity.is_active) {
             case 0:
@@ -25,6 +30,8 @@ function mountPage() {
                 break;
         }
 
+        div.appendChild(circle)
+        div.appendChild(paragraph)
         content.appendChild(div)
     })
 }
