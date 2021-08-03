8778874feb936c791399f29edc1b920d0a1594dc
Renato Novaes <renatoalmeida49@gmail.com>
2021-08-03 08:42:54 -0300

    feat(script.js): import of dark-mode module

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,5 @@
+import darkMode from './modules/dark-mode.js'
+
 let activities = []
 let data = document.querySelector("#data")
 let endpoint = 'http://localhost:5000/todos'
@@ -149,21 +151,6 @@ function addEventListeners() {
     })
     
     newTodo.addEventListener('keydown', addTodo)
-}
-
-function darkMode() {
-    let icon = document.querySelector("#dark-toggle")
-
-    icon.addEventListener('click', () => {
-        document.body.classList.toggle("dark-theme")
-        icon.classList.toggle("dark-theme")
-
-        if(icon.classList.contains('dark-theme')) {
-            icon.src = "./assets/images/icon-sun.svg"
-        } else {
-            icon.src = "./assets/images/icon-moon.svg"
-        }
-    })
 }
 
 fetchData()
